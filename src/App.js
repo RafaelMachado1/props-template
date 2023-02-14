33be3e1c4002c629a66af4d2a67ab5448493a84b
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2023-02-14 15:48:33 -0300

    pratica-guiada-concluida

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import Garagem from "./Componentes/Garagem";
 import "./styles.css";
 
 export default function App() {
+
+  const nome1 = "Rafael"
+
+  function apresentaGaragem1(){
+    alert(`Boas vindas à garagem de ${nome1}`)
+  }
+
+  const nome2 = "Gabriel"
+
+  function apresentaGaragem2(){
+    alert(`Boas vindas à garagem de ${nome2}`)
+  }
+
   return (
     <div>
-      <Garagem />
+      <Garagem nome={nome1} mensagemApresentacao={apresentaGaragem1} />
+      <Garagem nome={nome2} mensagemApresentacao={apresentaGaragem2} />
     </div>
   );
 }
